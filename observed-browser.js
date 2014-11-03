3e04f643425385ece4de5b0bafe68fee56d09cc4
Xiyang Chen <settinghead@gmail.com>
2014-11-03 16:52:54 -0500

    fake full-page oauth2 login interface
    - observed browserified
    - self-contained mock image encoding
    - remove unused mock
    - fake redirect-based Google Oauth2

---
+++ observed-browser.js
@@ -0,0 +1,656 @@
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+function EventEmitter() {
+  this._events = this._events || {};
+  this._maxListeners = this._maxListeners || undefined;
+}
+module.exports = EventEmitter;
+
+// Backwards-compat with node 0.10.x
+EventEmitter.EventEmitter = EventEmitter;
+
+EventEmitter.prototype._events = undefined;
+EventEmitter.prototype._maxListeners = undefined;
+
+// By default EventEmitters will print a warning if more than 10 listeners are
+// added to it. This is a useful default which helps finding memory leaks.
+EventEmitter.defaultMaxListeners = 10;
+
+// Obviously not all Emitters should be limited to 10. This function allows
+// that to be increased. Set to zero for unlimited.
+EventEmitter.prototype.setMaxListeners = function(n) {
+  if (!isNumber(n) || n < 0 || isNaN(n))
+    throw TypeError('n must be a positive number');
+  this._maxListeners = n;
+  return this;
+};
+
+EventEmitter.prototype.emit = function(type) {
+  var er, handler, len, args, i, listeners;
+
+  if (!this._events)
+    this._events = {};
+
+  // If there is no 'error' event listener then throw.
+  if (type === 'error') {
+    if (!this._events.error ||
+        (isObject(this._events.error) && !this._events.error.length)) {
+      er = arguments[1];
+      if (er instanceof Error) {
+        throw er; // Unhandled 'error' event
+      }
+      throw TypeError('Uncaught, unspecified "error" event.');
+    }
+  }
+
+  handler = this._events[type];
+
+  if (isUndefined(handler))
+    return false;
+
+  if (isFunction(handler)) {
+    switch (arguments.length) {
+      // fast cases
+      case 1:
+        handler.call(this);
+        break;
+      case 2:
+        handler.call(this, arguments[1]);
+        break;
+      case 3:
+        handler.call(this, arguments[1], arguments[2]);
+        break;
+      // slower
+      default:
+        len = arguments.length;
+        args = new Array(len - 1);
+        for (i = 1; i < len; i++)
+          args[i - 1] = arguments[i];
+        handler.apply(this, args);
+    }
+  } else if (isObject(handler)) {
+    len = arguments.length;
+    args = new Array(len - 1);
+    for (i = 1; i < len; i++)
+      args[i - 1] = arguments[i];
+
+    listeners = handler.slice();
+    len = listeners.length;
+    for (i = 0; i < len; i++)
+      listeners[i].apply(this, args);
+  }
+
+  return true;
+};
+
+EventEmitter.prototype.addListener = function(type, listener) {
+  var m;
+
+  if (!isFunction(listener))
+    throw TypeError('listener must be a function');
+
+  if (!this._events)
+    this._events = {};
+
+  // To avoid recursion in the case that type === "newListener"! Before
+  // adding it to the listeners, first emit "newListener".
+  if (this._events.newListener)
+    this.emit('newListener', type,
+              isFunction(listener.listener) ?
+              listener.listener : listener);
+
+  if (!this._events[type])
+    // Optimize the case of one listener. Don't need the extra array object.
+    this._events[type] = listener;
+  else if (isObject(this._events[type]))
+    // If we've already got an array, just append.
+    this._events[type].push(listener);
+  else
+    // Adding the second element, need to change to array.
+    this._events[type] = [this._events[type], listener];
+
+  // Check for listener leak
+  if (isObject(this._events[type]) && !this._events[type].warned) {
+    var m;
+    if (!isUndefined(this._maxListeners)) {
+      m = this._maxListeners;
+    } else {
+      m = EventEmitter.defaultMaxListeners;
+    }
+
+    if (m && m > 0 && this._events[type].length > m) {
+      this._events[type].warned = true;
+      console.error('(node) warning: possible EventEmitter memory ' +
+                    'leak detected. %d listeners added. ' +
+                    'Use emitter.setMaxListeners() to increase limit.',
+                    this._events[type].length);
+      if (typeof console.trace === 'function') {
+        // not supported in IE 10
+        console.trace();
+      }
+    }
+  }
+
+  return this;
+};
+
+EventEmitter.prototype.on = EventEmitter.prototype.addListener;
+
+EventEmitter.prototype.once = function(type, listener) {
+  if (!isFunction(listener))
+    throw TypeError('listener must be a function');
+
+  var fired = false;
+
+  function g() {
+    this.removeListener(type, g);
+
+    if (!fired) {
+      fired = true;
+      listener.apply(this, arguments);
+    }
+  }
+
+  g.listener = listener;
+  this.on(type, g);
+
+  return this;
+};
+
+// emits a 'removeListener' event iff the listener was removed
+EventEmitter.prototype.removeListener = function(type, listener) {
+  var list, position, length, i;
+
+  if (!isFunction(listener))
+    throw TypeError('listener must be a function');
+
+  if (!this._events || !this._events[type])
+    return this;
+
+  list = this._events[type];
+  length = list.length;
+  position = -1;
+
+  if (list === listener ||
+      (isFunction(list.listener) && list.listener === listener)) {
+    delete this._events[type];
+    if (this._events.removeListener)
+      this.emit('removeListener', type, listener);
+
+  } else if (isObject(list)) {
+    for (i = length; i-- > 0;) {
+      if (list[i] === listener ||
+          (list[i].listener && list[i].listener === listener)) {
+        position = i;
+        break;
+      }
+    }
+
+    if (position < 0)
+      return this;
+
+    if (list.length === 1) {
+      list.length = 0;
+      delete this._events[type];
+    } else {
+      list.splice(position, 1);
+    }
+
+    if (this._events.removeListener)
+      this.emit('removeListener', type, listener);
+  }
+
+  return this;
+};
+
+EventEmitter.prototype.removeAllListeners = function(type) {
+  var key, listeners;
+
+  if (!this._events)
+    return this;
+
+  // not listening for removeListener, no need to emit
+  if (!this._events.removeListener) {
+    if (arguments.length === 0)
+      this._events = {};
+    else if (this._events[type])
+      delete this._events[type];
+    return this;
+  }
+
+  // emit removeListener for all listeners on all events
+  if (arguments.length === 0) {
+    for (key in this._events) {
+      if (key === 'removeListener') continue;
+      this.removeAllListeners(key);
+    }
+    this.removeAllListeners('removeListener');
+    this._events = {};
+    return this;
+  }
+
+  listeners = this._events[type];
+
+  if (isFunction(listeners)) {
+    this.removeListener(type, listeners);
+  } else {
+    // LIFO order
+    while (listeners.length)
+      this.removeListener(type, listeners[listeners.length - 1]);
+  }
+  delete this._events[type];
+
+  return this;
+};
+
+EventEmitter.prototype.listeners = function(type) {
+  var ret;
+  if (!this._events || !this._events[type])
+    ret = [];
+  else if (isFunction(this._events[type]))
+    ret = [this._events[type]];
+  else
+    ret = this._events[type].slice();
+  return ret;
+};
+
+EventEmitter.listenerCount = function(emitter, type) {
+  var ret;
+  if (!emitter._events || !emitter._events[type])
+    ret = 0;
+  else if (isFunction(emitter._events[type]))
+    ret = 1;
+  else
+    ret = emitter._events[type].length;
+  return ret;
+};
+
+function isFunction(arg) {
+  return typeof arg === 'function';
+}
+
+function isNumber(arg) {
+  return typeof arg === 'number';
+}
+
+function isObject(arg) {
+  return typeof arg === 'object' && arg !== null;
+}
+
+function isUndefined(arg) {
+  return arg === void 0;
+}
+
+},{}],2:[function(require,module,exports){
+
+module.exports = exports = Change;
+
+/*!
+ * Change object constructor
+ *
+ * The `change` object passed to Object.observe callbacks
+ * is immutable so we create a new one to modify.
+ */
+
+function Change (path, change) {
+  this.path = path;
+  this.name = change.name;
+  this.type = change.type;
+  this.object = change.object;
+  this.value = change.object[change.name];
+  this.oldValue = change.oldValue;
+}
+
+
+},{}],3:[function(require,module,exports){
+
+/**
+ * Expose `debug()` as the module.
+ */
+
+module.exports = debug;
+
+/**
+ * Create a debugger with the given `name`.
+ *
+ * @param {String} name
+ * @return {Type}
+ * @api public
+ */
+
+function debug(name) {
+  if (!debug.enabled(name)) return function(){};
+
+  return function(fmt){
+    fmt = coerce(fmt);
+
+    var curr = new Date;
+    var ms = curr - (debug[name] || curr);
+    debug[name] = curr;
+
+    fmt = name
+      + ' '
+      + fmt
+      + ' +' + debug.humanize(ms);
+
+    // This hackery is required for IE8
+    // where `console.log` doesn't have 'apply'
+    window.console
+      && console.log
+      && Function.prototype.apply.call(console.log, console, arguments);
+  }
+}
+
+/**
+ * The currently active debug mode names.
+ */
+
+debug.names = [];
+debug.skips = [];
+
+/**
+ * Enables a debug mode by name. This can include modes
+ * separated by a colon and wildcards.
+ *
+ * @param {String} name
+ * @api public
+ */
+
+debug.enable = function(name) {
+  try {
+    localStorage.debug = name;
+  } catch(e){}
+
+  var split = (name || '').split(/[\s,]+/)
+    , len = split.length;
+
+  for (var i = 0; i < len; i++) {
+    name = split[i].replace('*', '.*?');
+    if (name[0] === '-') {
+      debug.skips.push(new RegExp('^' + name.substr(1) + '$'));
+    }
+    else {
+      debug.names.push(new RegExp('^' + name + '$'));
+    }
+  }
+};
+
+/**
+ * Disable debug output.
+ *
+ * @api public
+ */
+
+debug.disable = function(){
+  debug.enable('');
+};
+
+/**
+ * Humanize the given `ms`.
+ *
+ * @param {Number} m
+ * @return {String}
+ * @api private
+ */
+
+debug.humanize = function(ms) {
+  var sec = 1000
+    , min = 60 * 1000
+    , hour = 60 * min;
+
+  if (ms >= hour) return (ms / hour).toFixed(1) + 'h';
+  if (ms >= min) return (ms / min).toFixed(1) + 'm';
+  if (ms >= sec) return (ms / sec | 0) + 's';
+  return ms + 'ms';
+};
+
+/**
+ * Returns true if the given mode name is enabled, false otherwise.
+ *
+ * @param {String} name
+ * @return {Boolean}
+ * @api public
+ */
+
+debug.enabled = function(name) {
+  for (var i = 0, len = debug.skips.length; i < len; i++) {
+    if (debug.skips[i].test(name)) {
+      return false;
+    }
+  }
+  for (var i = 0, len = debug.names.length; i < len; i++) {
+    if (debug.names[i].test(name)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+/**
+ * Coerce `val`.
+ */
+
+function coerce(val) {
+  if (val instanceof Error) return val.stack || val.message;
+  return val;
+}
+
+// persist
+
+try {
+  if (window.localStorage) debug.enable(localStorage.debug);
+} catch(e){}
+
+},{}],"observed":[function(require,module,exports){
+// http://wiki.ecmascript.org/doku.php?id=harmony:observe
+
+var Change = require('./change');
+var Emitter = require('events').EventEmitter;
+var debug = require('debug')('observed');
+
+module.exports = exports = Observable;
+
+/**
+ * Observable constructor.
+ *
+ * The passed `subject` will be observed for changes to
+ * all properties, included nested objects and arrays.
+ *
+ * An `EventEmitter` will be returned. This emitter will
+ * emit the following events:
+ *
+ * - add
+ * - update
+ * - delete
+ * - reconfigure
+ *
+ * // - setPrototype?
+ *
+ * @param {Object} subject
+ * @param {Observable} [parent] (internal use)
+ * @param {String} [prefix] (internal use)
+ * @return {EventEmitter}
+ */
+
+function Observable (subject, parent, prefix) {
+  if ('object' != typeof subject)
+    throw new TypeError('object expected. got: ' + typeof subject);
+
+  if (!(this instanceof Observable))
+    return new Observable(subject, parent, prefix);
+
+  debug('new', subject, !!parent, prefix);
+
+  Emitter.call(this);
+  this._bind(subject, parent, prefix);
+};
+
+// add emitter capabilities
+for (var i in Emitter.prototype) {
+  Observable.prototype[i] = Emitter.prototype[i];
+}
+
+Observable.prototype.observers = undefined;
+Observable.prototype.onchange = undefined;
+Observable.prototype.subject = undefined;
+
+/**
+ * Binds this Observable to `subject`.
+ *
+ * @param {Object} subject
+ * @param {Observable} [parent]
+ * @param {String} [prefix]
+ * @api private
+ */
+
+Observable.prototype._bind = function (subject, parent, prefix) {
+  if (this.subject) throw new Error('already bound!');
+  if (null == subject) throw new TypeError('subject cannot be null');
+
+  debug('_bind', subject);
+
+  this.subject = subject;
+
+  if (parent) {
+    parent.observers.push(this);
+  } else {
+    this.observers = [this];
+  }
+
+  this.onchange = onchange(parent || this, prefix);
+  Object.observe(this.subject, this.onchange);
+
+  this._walk(parent || this, prefix);
+}
+
+/**
+ * Pending change events are not emitted until after the next
+ * turn of the event loop. This method forces the engines hand
+ * and triggers all events now.
+ *
+ * @api public
+ */
+
+Observable.prototype.deliverChanges = function () {
+  debug('deliverChanges')
+  this.observers.forEach(function(o) {
+    Object.deliverChangeRecords(o.onchange);
+  });
+}
+
+/**
+ * Walk down through the tree of our `subject`, observing
+ * objects along the way.
+ *
+ * @param {Observable} [parent]
+ * @param {String} [prefix]
+ * @api private
+ */
+
+Observable.prototype._walk = function (parent, prefix) {
+  debug('_walk');
+
+  var object = this.subject;
+
+  // keys?
+  Object.keys(object).forEach(function (name) {
+    var value = object[name];
+
+    if ('object' != typeof value) return;
+    if (null == value) return;
+
+    var path = prefix
+      ? prefix + '.' + name
+      : name;
+
+    new Observable(value, parent, path);
+  });
+}
+
+/**
+ * Stop listening to all bound objects
+ */
+
+Observable.prototype.stop = function () {
+  debug('stop');
+
+  this.observers.forEach(function (observer) {
+    Object.unobserve(observer.subject, observer.onchange);
+  });
+}
+
+/**
+ * Stop listening to changes on `subject`
+ *
+ * @param {Object} subject
+ * @api private
+ */
+
+Observable.prototype._remove = function (subject) {
+  debug('_remove', subject);
+
+  this.observers = this.observers.filter(function (observer) {
+    if (subject == observer.subject) {
+      Object.unobserve(observer.subject, observer.onchange);
+      return false;
+    }
+
+    return true;
+  });
+}
+
+/*!
+ * Creates an Object.observe `onchange` listener
+ */
+
+function onchange (parent, prefix) {
+  return function (ary) {
+    debug('onchange', prefix);
+
+    ary.forEach(function (change) {
+      var object = change.object;
+      var type = change.type;
+      var name = change.name;
+      var value = object[name];
+
+      var path = prefix
+        ? prefix + '.' + name
+        : name
+
+      if ('add' == type && null != value && 'object' == typeof value) {
+        new Observable(value, parent, path);
+      } else if ('delete' == type && 'object' == typeof change.oldValue) {
+        parent._remove(change.oldValue);
+      }
+
+      change = new Change(path, change);
+      parent.emit(type, change);
+      parent.emit(type + ' ' + path, change);
+      parent.emit('change', change);
+      parent.emit('change' + ' ' + path, change);
+    })
+  }
+}
+
+
+},{"./change":2,"debug":3,"events":1}]},{},[]);
